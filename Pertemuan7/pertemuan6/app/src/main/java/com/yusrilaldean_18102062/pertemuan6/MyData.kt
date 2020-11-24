6da3d630bc508c724985ea2e52187b630b5bf9ef
Yusril Aldean <45431496+Yusrilaldean@users.noreply.github.com>
2020-11-24 22:27:01 +0700

    Copy Project File Praktikum Modul 6

---
+++ Pertemuan7/pertemuan6/app/src/main/java/com/yusrilaldean_18102062/pertemuan6/MyData.kt
@@ -0,0 +1,11 @@
+package com.yusrilaldean_18102062.pertemuan6
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class MyData(
+    var name: String,
+    var description: String,
+    var photo: String
+) : Parcelable
