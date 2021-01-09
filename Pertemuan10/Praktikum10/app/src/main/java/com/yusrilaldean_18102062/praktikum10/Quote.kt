145a7481b845c8ccabc14e968c75754a10d626f9
Yusril Aldean <45431496+Yusrilaldean@users.noreply.github.com>
2021-01-09 23:00:17 +0700

    Pertemuan10

---
+++ Pertemuan10/Praktikum10/app/src/main/java/com/yusrilaldean_18102062/praktikum10/Quote.kt
@@ -0,0 +1,13 @@
+package com.yusrilaldean_18102062.praktikum10
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Quote(
+    var id: Int = 0,
+    var title: String? = null,
+    var description: String? = null,
+    var category: String? = null,
+    var date: String? = null
+) : Parcelable
